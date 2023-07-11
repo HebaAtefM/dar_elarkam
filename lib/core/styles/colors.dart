184dfa29634c9b736d9829d894553cc89f82f149
unknown <hebaatef962@yahoo.com>
2023-07-11 16:06:35 +0300

    a

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManger{
+  static const Color white=Color(0xffFFFFFF);
+  static const Color black=Color(0xff02001D);
+  static const Color dark2=Color(0xff3E3E46);
+  static const Color dark3=Color(0xff02001D);
+
+
+
+
+}
